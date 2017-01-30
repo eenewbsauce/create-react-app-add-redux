2dec5940837ce9c8b8946f82939dc11ae3428bd2
eenewbsauce <ryan.m.tracey@gmail.com>
2017-01-30 17:02:15 -0500

    feat(createDirectories): add spawn and dirs array

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 
 const spawn = require('child_process').spawn;
 const deps = ['redux', 'react-redux'];
+const dirs = ['actions'];
 
+//install dependecies
 const installDeps = deps.forEach(dep => {
   spawn('npm', ['i', '--save', dep]);
+});
+
+//create directories
+const createDirectories = dirs.forEach(dir => {
+  spawn('mkdir', [`src/${dir}`]);
 });
